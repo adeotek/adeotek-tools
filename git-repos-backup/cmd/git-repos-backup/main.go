267cb127f1a0bdd8290875c9a4e38a4c72c02fbb
George Benjamin-Schonberger <george.benjamin@gmail.com>
2025-04-27 16:04:16 +0300

    [fix:git-repos-backup] GitHub action fix

---
+++ git-repos-backup/cmd/git-repos-backup/main.go
@@ -0,0 +1,10 @@
+// Package main provides the entry point for the git-repos-backup application
+package main
+
+import (
+	"github.com/adeotek/adeotek-tools/git-repos-backup/internal/app"
+)
+
+func main() {
+	app.Run()
+}
